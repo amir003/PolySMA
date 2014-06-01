6bf933f7171066f261071da4ac4e76452be8c7ce
amir003 <amirnaar@hotmail.fr>
2014-06-01 19:51:15 +0200

    Update grid.py

--- grid.py
+++ grid.py
@@ -9,8 +9,8 @@ class SimpleGrid(wx.grid.Grid):
         wx.grid.Grid.__init__(self, parent, -1)
         self.CreateGrid(50, 50)
         
-        self.SetRowLabelSize(50)
-        self.SetColLabelSize(30)#size of a col label (height to be precise) and so the col
+        self.SetRowLabelSize(20)
+        self.SetColLabelSize(20)#size of a col label (height to be precise) and so the col
         
         #avoid resize moification by the user of the grid
         self.EnableDragColSize(0)
@@ -20,7 +20,7 @@ class SimpleGrid(wx.grid.Grid):
         #set the size of all the col
         for i in range(50):
           self.SetColLabelValue(i, str(i+1))#set the label
-          self.SetColSize(i, 30)#set the size of all the col
+          self.SetColSize(i, 20)#set the size of all the col
         
         self.SetCellValue(0, 0, "¥")
         self.SetCellTextColour(0,0,'red') #colorie le texte
