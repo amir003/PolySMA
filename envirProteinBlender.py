0ec059d3ddb33b9e30ca028183a57408cef7851b
Emilie Drouineau <edroui800e@emi.u-bordeaux1.fr>
2014-06-30 16:27:39 +0200

    réorganisation du code

--- envirProteinBlender.py
+++ envirProteinBlender.py
@@ -6,30 +6,6 @@ import re
 import bmesh
 from bpy import context as C
 
-
-#print("coucou")
-print(bpy.ops.object.mode)
-bpy.ops.object.mode_set(mode='OBJECT')
-
-# remove mesh Cube and Sphere before new run
-for i in bpy.data.objects:
-    if re.match(r'.*Sphere.*',i.name,re.M|re.I):
-        obj=bpy.data.objects[i.name]
-        obj.select=True
-        bpy.ops.object.delete(use_global=False)
-    if re.match(r'.*Cube.*',i.name,re.M|re.I):
-        obj=bpy.data.objects[i.name]
-        obj.select=True
-        bpy.ops.object.delete(use_global=False)
-
-#gerer ca en dehors de blender
-#gros pb avec les redirections des sorties standards
-#print ("taille de l'environnement")
-#a=input()
-
-#stocker dans une liste les coord
-#verrifier que 2 coord sont bien differentes
-#puis travailler sur les interactions
 def choixEnviron():
     print("Taille de l'environnement")
     envSize=50
@@ -38,36 +14,40 @@ def choixEnviron():
     return envSize,nbProt
 
 
-def initProteine(envSize,nbProt):
-    dicoProt={}
+def initProteine(envSize,nbProt,scaleFactor):
+    listProt=[]
     for i in range(0,nbProt,1):
-        caseLibre=0
-        while caseLibre==0:
-            listCoord=[]
+        caseEmpty=False
+        while !caseEmpty:
             x=random.randint(1,envSize)
-            x=float(x)/10
+            x=float(x)/scaleFactor
             y=random.randint(1,envSize)
-            y=float(y)/10
+            y=float(y)/scaleFactor
             z=random.randint(1,envSize)
-            z=float(z)/10
+            z=float(z)/scaleFactor
             listCoord=[x,y,z]
-            caseLibre=voisinInit(dicoProt,listCoord)
-            if caseLibre==1:
-                #creation d'une sphere
-                nameS="Sphere"+str(i)
-                dicoProt[nameS]=listCoord
-                bpy.ops.mesh.primitive_uv_sphere_add(segments=16, size=0.05, view_align=False, enter_editmode=False, location=(x, y, z), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
-                bpy.context.object.name = nameS
-                #creation des poles:
-                obj=bpy.data.objects[nameS]
-                obj.select=True
-                poleProteine(obj,nameS)
-                #appli d'une rotation aleatoire
-                a,b,c=angleRotation()
-                obj=bpy.data.objects[nameS]
-                obj.select=True
-                bpy.ops.transform.rotate(axis=(a, b, c))
-    return dicoProt
+            caseEmpty=caseIsEmpty(listProt,listCoord)
+            if caseEmpty:
+                drawProt()
+    return listProt
+
+def drawProt(listProt,listCoord,scaleFactor):
+    i=len(listProt)
+    #creation d'une sphere
+    nameS="Sphere"+str(i)
+    currentSphere=bpy.ops.mesh.primitive_uv_sphere_add(segments=16, size=0.5/scaleFactor, location=(x, y, z), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
+    bpy.context.object.name = nameS
+    #creation des poles:
+    currentProt=[]
+    listObjOfCurrentProt=poleProteine(obj,nameS)
+    currentProt.append(listObjOfCurrentProt)
+    currentProt.append(listCoord)
+    #appli d'une rotation aleatoire
+    a,b,c=angleRotation()
+    for i in range(2):
+        currentPole=listObjOfCurrentProt[i]
+        cuyrrentPole.select=True
+        bpy.ops.transform.rotate(axis=(a, b, c))
 
 def angleRotation():
     a=random.randint(-360,360)
@@ -78,50 +58,49 @@ def angleRotation():
     c=c*(math.pi/180)
     return a,b,c
 
-def voisinInit(dicoProt,listCoord):
-    caseLibre=1
-    for key in dicoProt:
-        listTest=dicoProt[key]
+def caseIsEmpty(listProt,listCoord):
+    for protToCheck in listProt:
+        listCoordToCheck=protToCheck[1]
         if listTest==listCoord:
-            caseLibre=0  
-            break
-    return caseLibre
+            return False
+    return True
   
-def poleProteine(obj,name):
+def poleProteine(objectName):
+    sphere=bpy.data.objects[objectName]
+    sphere.select=True
+    #besoin de passer en "edit" mode
     bpy.ops.object.mode_set(mode='EDIT')
+    #passage en structure de pointeurs 
     bm = bmesh.from_edit_mesh(C.object.data)
     edges = []
 
-    #coupe parallele a l'axe des y
+    #coupe en 2 parallele a l'axe des y
     for i in range(-10, 10, 2):
         ret = bmesh.ops.bisect_plane(bm, geom=bm.verts[:]+bm.edges[:]+bm.faces[:], plane_co=(i,0,0), plane_no=(-1,0,0))
         bmesh.ops.split_edges(bm, edges=[e for e in ret['geom_cut'] if isinstance(e, bmesh.types.BMEdge)])
 
+    #met a jour les pointeurs
     bmesh.update_edit_mesh(C.object.data)
-    
+    #separe en 2 objets
     bpy.ops.mesh.separate(type='LOOSE')
-    
+    #repasse en mode objet
     bpy.ops.object.mode_set(mode='OBJECT')
-    obj=bpy.data.objects[name]
-    obj.select=True
-    red = makeMaterial('Green', (0,1,0), (1,1,1), 1)
-    setMaterial(bpy.context.object, red)
-    
-    nameS=name+'.001'
-    #print(nameS)
+    #selection objet
+    pole1=bpy.data.objects[objectName]
+    pole1.select=True
+    #colorie en vert
+    green = makeMaterial('Green', (0,1,0), (1,1,1), 1)
+    pole1.data.materials.append(green)
+    #setMaterial(bpy.context.object, green)
+    #selection du pole 2
+    namePole2=objectName+'.001'
+    pole2=bpy.data.objects[namePole2]
+    pole2.select=True
+    #colorie en bleu
     blue = makeMaterial('BlueSemi', (0,0,1), (0.5,0.5,0), 0.5)
-    obj=bpy.data.objects[nameS]
-    obj.select=True
-    me = obj.data
-    #me.materials.append(red)
-    me.materials.append(blue)
-    #sme.materials.append(yellow)
-    #blue = makeMaterial('BlueSemi', (0,0,1), (0.5,0.5,0), 0.5)
-    #setMaterial(bpy.context.object, red)
-    #bpy.ops.object.delete(use_global=False)
-    bpy.ops.object.join()
-
-
+    pole2.data.materials.append(blue)
+    #bpy.ops.object.join()#pour joindre les deux demi-spheres
+    return [pole1,pole2]
 
 def makeMaterial(name, diffuse, specular, alpha):
     mat = bpy.data.materials.new(name)
@@ -180,6 +159,7 @@ def mouvementProt(envSize):
         print("x",x,"y",y,"z",z)
         bpy.context.scene.frame_set(frame_num)
         for prot in bpy.data.objects:
+            
             bool=voisin(prot.location[0]+x,prot.location[1]+y,prot.location[2]+z)
             
             prot.location[0]=prot.location[0]+x
@@ -199,26 +179,24 @@ def mouvementProt(envSize):
                 prot.location[2]=1
             prot.keyframe_insert(data_path="location", index=-1)
         frame_num+=10
-        
-    #positions = (0,0,2),(0,1,2),(3,2,1),(3,4,1),(1,2,1)
-    
-    #start_pos = (0,0,0)
-    #bpy.ops.mesh.primitive_uv_sphere_add(segments=32, size=0.3, location=start_pos)
-    #bpy.ops.object.shade_smooth()
-    #obj=bpy.data.objects["Sphere1"]
-    #obj.select=True
-    
-    #ob = bpy.context.active_object
+       
+def clean():
+    # remove mesh Cube and Sphere before new run
+    for i in bpy.data.objects:
+        if re.match(r'.*Sphere.*',i.name,re.M|re.I):
+            obj=bpy.data.objects[i.name]
+            obj.select=True
+            bpy.ops.object.delete(use_global=False)
+        if re.match(r'.*Cube.*',i.name,re.M|re.I):
+            obj=bpy.data.objects[i.name]
+            obj.select=True
+            bpy.ops.object.delete(use_global=False)
 
-    #frame_num = 0
- 
-#    for position in positions:
-#        bpy.context.scene.frame_set(frame_num)
-#        ob.location = position
-#        ob.keyframe_insert(data_path="location", index=-1)
-#        frame_num += 10
-
+#prog principal
+print(bpy.ops.object.mode)
+bpy.ops.object.mode_set(mode='OBJECT')
+clean()
+scaleFactor=10
 envSize,nbProt=choixEnviron()
-dico=initProteine(envSize,nbProt)
-mouvementProt(envSize)
-                                                                                                                                                                                                                                                                                  
+listProt=initProteine(envSize,nbProt,scaleFactor)
+mouvementProt(listProt,envSize)                                                                                                                                                                                                                                                                                 
