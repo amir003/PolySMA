b668fdf5237898158294a31f27578d25d5279fcf
Emilie Drouineau <edroui800e@emi.u-bordeaux1.fr>
2014-07-09 16:06:03 +0200

    mis en place de l'attachement

--- envirProteinBlender.py
+++ envirProteinBlender.py
@@ -5,17 +5,46 @@ import math
 import re
 import bmesh
 from bpy import context as C
-
+                                                                                                                                                                                         
+#determination des parametres de l'environnement
+#def choixEnviron():
+    #file = open("settings.txt", "r")
+    #lines = file.readlines()
+    #for line in lines :
+        #line=float(line.split(": ")[1].split("#")[0])
+    #return lines
 def choixEnviron():
     print("Taille de l'environnement")
     envSize=50
     print("nombre de proteine dans l'environnement : concentration")
-    nbProt=200
+    nbProt=100
     print("rayon de la proteine")
     
     return envSize,nbProt
 
-
+#classe grain biologique
+class BiologicalGrain:
+    def __init__(self, coordinatesP, coordinatesR,listOfNeighbours,associationProbability ):
+        self.x, self.y, self.z = coordinatesP
+        self.rotateX, self.rotateY, self.rotateZ = coordinatesR
+        self.associationProb = associationProbability
+        self.associatedNeighbours = listOfNeighbours
+    def getCoordP(self):
+        listCoord=[self.x,self.y,self.z]
+        return listCoord
+    def getNeighbours():
+        return self.associatedNeighbours
+        
+#classe grain biologique
+class SimulatedGrain:
+    def __init__(self, bGrain,activesFace, grainName):
+        self.BiologicalGrain = bGrain
+        self.activesFaces =  activesFace      
+        self.moved=False
+        self.name=grainName
+        self.attach=False
+        
+# initialisation de l'environnement en terme de coordonnees des objets
 def initProteine(envSize,nbProt,scaleFactor,grainDiameter):
     listProt=[]
     for i in range(0,nbProt,1):
@@ -27,41 +56,32 @@ def initProteine(envSize,nbProt,scaleFactor,grainDiameter):
             y=float(y)/scaleFactor
             z=random.randrange(0,envSize,grainDiameter)
             z=float(z)/scaleFactor
-            #print("init x",x,"y",y,"z",z)
             listCoord=[x,y,z]
             caseEmpty=caseIsEmpty(listProt,listCoord)
             if caseEmpty:
-                #print(listCoord)
                 drawProt(listProt,listCoord,scaleFactor,grainDiameter)
     return listProt
 
+# dessiner le proteine dans l'environnement blender : rotation, faces d'interet (site de liaison)...
 def drawProt(listProt,listCoord,scaleFactor,grainDiameter):
-    #print("i",i)
     x,y,z=listCoord
     #creation d'une sphere
     nameS="Sphere"+str(len(listProt))
-    bpy.ops.mesh.primitive_uv_sphere_add(segments=16, size=grainDiameter/(scaleFactor*2), view_align=False, enter_editmode=False, location=(x, y, z), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
+    bpy.ops.mesh.primitive_ico_sphere_add(size=grainDiameter/(scaleFactor*2), view_align=False, enter_editmode=False, location=(x, y, z), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
     bpy.context.object.name = nameS
-    obj=bpy.data.objects[nameS]
-    ix=obj.location[0]
-    iy=obj.location[1]
-    iz=obj.location[2]
-    print(nameS," x",ix,"y",iy,"z",iz)
-    print("liste",listCoord)
-    print("\n")
     #creation des poles:
-    currentProt=[]
-    listObjOfCurrentProt=poleProteine(nameS)
-    currentProt.append(listObjOfCurrentProt)
-    currentProt.append(listCoord)
+    proteinFaces(nameS)
     #appli d'une rotation aleatoire
     a,b,c=angleRotation()
-    for i in range(2):
-        currentPole=listObjOfCurrentProt[i]
-        currentPole.select=True
-        bpy.ops.transform.rotate(axis=(a, b, c))
-    listProt.append(currentProt)
+    listRotate=[a,b,c]
+    bpy.ops.transform.rotate(axis=(a, b, c))
+    #creation des grains
+    listNeighbours=[]
+    protein=BiologicalGrain(listCoord,listRotate,listNeighbours,2)
+    grain=SimulatedGrain(protein,1,nameS)
+    listProt.append(grain)
 
+# determination aleatoire de l'angle de rotation de la proteine
 def angleRotation():
     a=random.randint(-360,360)
     a=a*(math.pi/180)
@@ -71,157 +91,146 @@ def angleRotation():
     c=c*(math.pi/180)
     return a,b,c
 
+# verrification pour savoir si une case d'interet est vide
 def caseIsEmpty(listProt,listCoord):
     for protToCheck in listProt:
-        listCoordToCheck=protToCheck[1]
-        #print("Coor a tester ",listCoord," coord deja existante ",listCoordToCheck)
+        listCoordToCheck=protToCheck.BiologicalGrain.x,protToCheck.BiologicalGrain.y,protToCheck.BiologicalGrain.z
         if listCoordToCheck==listCoord:
-            #print("Danger")
             return False
     return True
   
-def poleProteine(objectName):
-    sphere=bpy.data.objects[objectName]
-    sphere.select=True
-    #besoin de passer en "edit" mode
-    bpy.ops.object.mode_set(mode='EDIT')
-    #passage en structure de pointeurs 
-    bm = bmesh.from_edit_mesh(C.object.data)
-    edges = []
-
-    #coupe en 2 parallele a l'axe des y
-    for i in range(-10, 10, 2):
-        ret = bmesh.ops.bisect_plane(bm, geom=bm.verts[:]+bm.edges[:]+bm.faces[:], plane_co=(i,0,0), plane_no=(-1,0,0))
-        bmesh.ops.split_edges(bm, edges=[e for e in ret['geom_cut'] if isinstance(e, bmesh.types.BMEdge)])
-
-    #met a jour les pointeurs
-    bmesh.update_edit_mesh(C.object.data)
-    #separe en 2 objets
-    bpy.ops.mesh.separate(type='LOOSE')
-    #repasse en mode objet
-    bpy.ops.object.mode_set(mode='OBJECT')
-    #selection objet
-    pole1=bpy.data.objects[objectName]
-    pole1.select=True
-    #colorie en vert
-    green = makeMaterial('Green', (0,1,0), (1,1,1), 1)
-    pole1.data.materials.append(green)
-    #setMaterial(bpy.context.object, green)
-    #selection du pole 2
-    namePole2=objectName+'.001'
-    pole2=bpy.data.objects[namePole2]
-    pole2.select=True
-    #colorie en bleu
-    blue = makeMaterial('BlueSemi', (0,0,1), (0.5,0.5,0), 0.5)
-    pole2.data.materials.append(blue)
-    pole1.keyframe_insert(data_path="location", index=-1)
-    pole2.keyframe_insert(data_path="location", index=-1)
-    #bpy.ops.object.join()#pour joindre les deux demi-spheres
-    return [pole1,pole2]
-
-def makeMaterial(name, diffuse, specular, alpha):
-    mat = bpy.data.materials.new(name)
-    mat.diffuse_color = diffuse
-    mat.diffuse_shader = 'LAMBERT' 
-    mat.diffuse_intensity = 1.0 
-    mat.specular_color = specular
-    mat.specular_shader = 'COOKTORR'
-    mat.specular_intensity = 0.5
-    mat.alpha = alpha
-    mat.ambient = 1
-    return mat
- 
-def setMaterial(ob, mat):
+# determination des faces correspondantes aux sites de liaison
+def proteinFaces(objectName):
+        #def obj + face a colorier et attribuer une propriete
+    obj=bpy.data.objects[objectName]
+    obj.select=True
+    faces=obj.data.polygons
+    obj.data.calc_tessface()
+    faces=obj.data.tessfaces
+    
+    red = bpy.data.materials.new('Red')
+    red.diffuse_color = (1,0,0)
+    blue = bpy.data.materials.new('Blue')
+    blue.diffuse_color = (0,0,1)
+    yellow = bpy.data.materials.new('Yellow')
+    yellow.diffuse_color = (1,1,0)
+    grey = bpy.data.materials.new('Grey')
+    grey.diffuse_color = (0.5,0.5,0.5)
+    
+    ob = bpy.context.object
     me = ob.data
-    me.materials.append(mat)
-        
-def voisin(x,y,z):
-    caseVide=1
-    for obj in bpy.data.objects:
-        #obj=bpy.data.objects[i]
-        ix=obj.location[0]
-        iy=obj.location[1]
-        iz=obj.location[2]
-        if (x==ix and y==iy and z==iz):
-            caseVide=0
-    return caseVide
-
+    me.materials.append(grey)
+    me.materials.append(red)
+    me.materials.append(blue)
+    me.materials.append(yellow)
+    
+  # Assign materials to faces
+    for f in faces:
+        #print(f.index)
+        if f.index==78 or f.index==79:
+            f.material_index=2
+        elif f.index==10 or f.index==5:
+            f.material_index=1
+        elif f.index==0 or f.index==2:
+            f.material_index=3
+        else:
+            f.material_index=0
+        #f.material_index = f.index % 3
+ 
+    # Set left half of sphere smooth, right half flat shading
+   
+# determiner les molecules attachees en fonction de la distance entre les sites de liaisons
 def attacher(listProt,grainDiameter,scaleFactor):
     #trouver les spheres attachées
-    portLiees={}
+    #portLiees={}
     distance=grainDiameter/scaleFactor
     for i in range(len(listProt)-1):
-        #print(len(listProt))
-        #print("coucou")
-        x1=listProt[i][1][0]
-        y1=listProt[i][1][1]
-        z1=listProt[i][1][2]
+        x1=listProt[i].BiologicalGrain.x
+        y1=listProt[i].BiologicalGrain.y
+        z1=listProt[i].BiologicalGrain.z
         for j in range(i+1,len(listProt)):
-            x2=listProt[j][1][0]
-            y2=listProt[j][1][1]
-            z2=listProt[j][1][2]
+            x2=listProt[j].BiologicalGrain.x
+            y2=listProt[j].BiologicalGrain.y
+            z2=listProt[j].BiologicalGrain.z
             dx=x1-x2
             dy=y1-y2
             dz=z1-z2
             if (dx <distance and dx >-distance) and (dy <distance and dy >-distance) and (dz <distance and dz >-distance):
                 print ("proteine attachee !!!!!!!!!!!!!",dx,dy,dz)
+                listProt[i].BiologicalGrain.associatedNeighbours.append(listProt[j])
+                listProt[j].BiologicalGrain.associatedNeighbours.append(listProt[i])
 
+# determiner les coord de chaque molecules en fonction de chaque frame
 def mouvementProt(listProt,envSize,scaleFactor,grainDiameter):
     envSize=envSize/scaleFactor
-    frame_num =10#numero de la frame (pour le temps)
-    for position in range(1):
-        bpy.context.scene.frame_set(frame_num)
-        x=random.randint(-grainDiameter,grainDiameter)
-        x=x/scaleFactor
-        y=random.randint(-grainDiameter,grainDiameter)
-        y=y/scaleFactor
-        z=random.randint(-grainDiameter,grainDiameter)
-        z=z/scaleFactor
-        #1print("x",x,"y",y,"z",z)
+    frame_num =0#numero de la frame (pour le temps)
+    # Verrif que le bool est false (pas encore deplace)
+    attacher(listProt,grainDiameter,scaleFactor)
+    nbDeplacement=26
+    for position in range(nbDeplacement):
+        indiceList=indice(listProt)
+        while len(indiceList)!=0:
+            # choix d'une molecule aleatoirement
+            i=random.randint(0,len(indiceList)-1)
+            ind=indiceList[i]
+            prot=listProt[ind]
+            # enregistrement des donnees sur la frame
+            bpy.context.scene.frame_set(frame_num)
+            mvtOK=False
+            if prot.moved==True:
+                mvtOK=True
+                indiceList.pop(i)
+            while mvtOK!=True:
+                # calcul du mouvement
+                x=random.randrange(-grainDiameter,grainDiameter,grainDiameter)
+                x=x/scaleFactor
+                y=random.randrange(-grainDiameter,grainDiameter,grainDiameter)
+                y=y/scaleFactor
+                z=random.randrange(-grainDiameter,grainDiameter,grainDiameter)
+                z=z/scaleFactor
+                move=[x,y,z]
+                # tests pour controler le mouvement
+                coordOfCurrentSphere = prot.BiologicalGrain.x,prot.BiologicalGrain.y,prot.BiologicalGrain.z
+                listCoord=[coordOfCurrentSphere[0]+x,coordOfCurrentSphere[1]+y,coordOfCurrentSphere[2]+z]
+                moveIsPossible=caseIsEmpty(listProt,listCoord)
+                if moveIsPossible==True:
+                    mvtOK=True
+                    deplacePosition(prot, x, y, z, scaleFactor)
+                    indiceList.pop(i)
         for prot in listProt:
-            objectsOfCurrentSphere = prot[0]
-            coordOfCurrentSphere = prot[1]
-            listCoord=[coordOfCurrentSphere[0]+x,coordOfCurrentSphere[1]+y,coordOfCurrentSphere[2]+z]
-            moveIsPossible=caseIsEmpty(listProt,listCoord)
-            deplacePosition(prot, x, y, z,scaleFactor)
-            coordOfCurrentSphere =listCoord
+            prot.moved=False
         frame_num+=10
 
-def deplacePosition(prot, x, y, z,scaleFactor):
-    objectsOfCurrentSphere = prot[0]
-    for i in range(2):
-        currentPole=objectsOfCurrentSphere[i]
-        currentPole.location[0]=currentPole.location[0]+x
-        currentPole.location[1]=currentPole.location[1]+y
-        currentPole.location[2]=currentPole.location[2]+z
-        if currentPole.location[0]<0:
-            currentPole.location[0]=envSize/scaleFactor
-        if currentPole.location[1]<0:
-            currentPole.location[1]=envSize/scaleFactor
-        if currentPole.location[2]<0:
-            currentPole.location[2]=envSize/scaleFactor
-        if currentPole.location[0]>envSize/scaleFactor:
-            currentPole.location[0]=0
-        if currentPole.location[1]>envSize/scaleFactor:
-            currentPole.location[1]=0
-        if currentPole.location[2]>envSize/scaleFactor:
-            currentPole.location[2]=0
-        currentPole.keyframe_insert(data_path="location", index=-1)
-   
-def clean():
-    # remove mesh Cube and Sphere before new run
-    for i in bpy.data.objects:
-        print(i.name)
-        if re.match(r'.*Sphere.*',i.name,re.M|re.I):
-            obj=bpy.data.objects[i.name]
-            obj.select=True
-            bpy.ops.object.delete(use_global=False)
-        if re.match(r'.*Cube.*',i.name,re.M|re.I):
-            obj=bpy.data.objects[i.name]
-            obj.select=True
-            bpy.ops.object.delete(use_global=False)
+def indice(listProt):
+    indiceList=[]
+    for i in range(len(listProt)):
+        indiceList.append(i)
+    return indiceList
+           
+# l'environnement est considere comme inifini
+def deplacePosition(prot, x, y, z, scaleFactor):
+    bProt=bpy.data.objects[prot.name]
+    bProt.location[0]=bProt.location[0]+x
+    bProt.location[1]=bProt.location[1]+y
+    bProt.location[2]=bProt.location[2]+z
+    if bProt.location[0]<0:
+        bProt.location[0]=envSize/scaleFactor
+    if bProt.location[1]<0:
+        bProt.location[1]=envSize/scaleFactor
+    if bProt.location[2]<0:
+        bProt.location[2]=envSize/scaleFactor
+    if bProt.location[0]>envSize/scaleFactor:
+        bProt.location[0]=0
+    if bProt.location[1]>envSize/scaleFactor:
+        bProt.location[1]=0
+    if bProt.location[2]>envSize/scaleFactor:
+        bProt.location[2]=0
+    bProt.keyframe_insert(data_path="location", index=-1)
+    prot.BiologicalGrain.x,prot.BiologicalGrain.y,prot.BiologicalGrain.z=bProt.location[0],bProt.location[1],bProt.location[2]
+    prot.moved=True
 
-def clean2():
+#supprimer tous les objets de l'environnement avant de refaire une simulation   
+def clean():
     for obj in bpy.data.objects:
         currentName=obj.name
         #print(currentName)
@@ -230,46 +239,16 @@ def clean2():
             bpy.ops.object.delete(use_global=False)
  
  
-#classe grain biologique
-class biologicalGrain:
-    def __init__(self, coordinates,listOfNeighbours,associationProbability ):
-        self.x, slef.y, self.z = coordinates
-        self.associationProb = associationProbability
-        self.associatedNeighbours = listOfNeighbours
-        
-#classe grain biologique
-class simulatedGrain:
-    def __init__(self, bGrain,activesFace):
-        self.biologicalGrain = bGrain
-        self.activesFaces =  activesFace      
-        self.moved=False
 
 #prog principal
-#filename = "testEnvirProteinV4.py"
-#exec(compile(open(filename).read(), filename, 'exec'))
-
-#print(bpy.ops.object.mode)
 bpy.ops.object.mode_set(mode='OBJECT')
-#clean()
-clean2()
+clean()
 scaleFactor = 10
 grainDiameter = 2
+#grainDiameter,envSize,nbProt,associationProb,nbRunPerFrame,sizeActiveSite,nbActivesSites=choixEnviron()
 envSize,nbProt=choixEnviron()
 bpy.context.scene.frame_set(0)
 listProt=initProteine(envSize,nbProt,scaleFactor,grainDiameter)
-print(listProt)
 mouvementProt(listProt,envSize,scaleFactor,grainDiameter)
-#attacher(listProt,grainDiameter,scaleFactor) 
-        
-for i in range(len(listProt)-1):
-    protCoord=listProt[i][1]
-    x1=protCoord[0]
-    y1=protCoord[1]
-    z1=protCoord[2]
-    for j in range(i+1,len(listProt)):
-        protTest=listProt[j][1]
-        x2=protTest[0]
-        y2=protTest[1]
-        z2=protTest[2]
-        if x1==x2 and y1==y2 and z1==z2:
-            print("danger !!!!!!!!!!!!")                                                                                                                                                                                                                                                                         
+#attacher(listProt,grainDiameter,scaleFactor)   
+
